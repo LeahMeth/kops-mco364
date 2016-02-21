4d3d07863f7875f928368bbab9bcbd9ffefd2458
leahkops <leahkops1@gmail.com>
2016-02-21 15:05:58 -0500

    added paint function

--- src/main/java/kops/mco364/paint/PaintFrame.java
+++ src/main/java/kops/mco364/paint/PaintFrame.java
@@ -2,11 +2,22 @@ package kops.mco364.paint;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 
-import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class PaintFrame extends JFrame {
+
+	private JButton print;
+	private Canvas canvas;
 
 	public PaintFrame() {
 		setTitle("Paint");
@@ -17,7 +28,40 @@ public class PaintFrame extends JFrame {
 		container = getContentPane();
 		container.setLayout(new BorderLayout());
 
-		Canvas canvas = new Canvas();
+		canvas = new Canvas();
+
+		print = new JButton("Print Canvas");
+		print.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				PrinterJob pj = PrinterJob.getPrinterJob();
+				pj.setJobName("Print Canvas");
+				pj.setPrintable(new Printable() {
+
+					public int print(Graphics graphics, PageFormat format,
+							int pageIndex) throws PrinterException {
+						if(pageIndex > 0){
+							return Printable.NO_SUCH_PAGE;
+						}
+						Graphics2D graphics2 = (Graphics2D) graphics;
+						graphics2.translate(format.getImageableX(), format.getImageableY());
+						canvas.paint(graphics2);
+						return Printable.PAGE_EXISTS;
+						
+					}
+
+				});
+				try {
+					pj.print();
+				} catch (PrinterException e) {
+					e.printStackTrace();
+				}			
+
+			}
+
+		});
+
+		container.add(print, BorderLayout.NORTH);
 		container.add(canvas, BorderLayout.CENTER);
 
 	}
